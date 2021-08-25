edb975c846e7e365dc5f478bf205bff465e77add
doctoryearali <82830552+doctoryearali@users.noreply.github.com>
2021-08-25 10:26:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function draw()
+{
+
+}
+
+function setup()
+{
+ Canvas=createcanvas(300,300);
+ canvas.center;
+}
+
+function save_img_btn()
+{
+ save("Mustache.png")
+}
